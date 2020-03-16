1fd27220a9af5026c9ad1cbc23e1ce0bdaccd45d
Igor Lukashevich <igorek0320@gmail.com>
2020-03-16 10:31:50 +0300

    Migration fix duplicate key

--- database/migrations/2020_03_04_131751_add_foreing_key_to_user_role_table.php
+++ database/migrations/2020_03_04_131751_add_foreing_key_to_user_role_table.php
@@ -14,8 +14,8 @@ class AddForeingKeyToUserRoleTable extends Migration
     public function up()
     {
         Schema::table('user_role', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //$table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
@@ -27,8 +27,8 @@ class AddForeingKeyToUserRoleTable extends Migration
     public function down()
     {
         Schema::table('user_role', function (Blueprint $table) {
-            $table->dropForeign('user_role_role_id_foreign');
-            $table->dropForeign('user_role_user_id_foreign');
+          //  $table->dropForeign('user_role_role_id_foreign');
+           // $table->dropForeign('user_role_user_id_foreign');
         });
     }
 }
